35dc2eefd398f90308e2a71f187da81109dd49de
dn <dawid.nowicki@protonmail.com>
2023-11-01 19:43:42 +0100

    added angular router and examples components

--- wareVoiceWeb/src/app/app.module.ts
+++ wareVoiceWeb/src/app/app.module.ts
@@ -3,13 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
+import { RouterModule } from '@angular/router';
+import { WarehouseComponent } from './warehouse/warehouse.component';
+import { NavbarComponent } from './common/navbar/navbar.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WarehouseComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
+    RouterModule.forRoot([
+      {path: 'warehouse', component: WarehouseComponent},
+    ]),
     BrowserAnimationsModule
   ],
   providers: [],

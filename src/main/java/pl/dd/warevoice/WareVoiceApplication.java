d0661b3782d39ea156d4a39755cc1d2ad441c053
dn <dawid.nowicki@protonmail.com>
2023-11-01 19:17:43 +0100

    init

---
+++ src/main/java/pl/dd/warevoice/WareVoiceApplication.java
@@ -0,0 +1,13 @@
+package pl.dd.warevoice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WareVoiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WareVoiceApplication.class, args);
+    }
+
+}
